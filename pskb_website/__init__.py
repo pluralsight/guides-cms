770b6e7eabf6ae2f9a76ec9dac1e94dded6fc641
Luke Lee <durdenmisc@gmail.com>
2015-11-11 10:05:45 +0100

    Remove last remnants of db requirements

--- pskb_website/__init__.py
+++ pskb_website/__init__.py
@@ -1,7 +1,6 @@
 import os
 
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -28,7 +27,5 @@ else:
     app.config.from_object(os.environ['APP_SETTINGS'])
     app.secret_key = app.config['SECRET_KEY']
 
-
-db = SQLAlchemy(app)
 
 import pskb_website.views
