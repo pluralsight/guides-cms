03a732a2da204692c6ea54a2c3d693815371e03f
Luke Lee <durdenmisc@gmail.com>
2015-11-27 15:18:18 +0100

    Improve remote layer with logging

--- pskb_website/remote.py
+++ pskb_website/remote.py
@@ -28,6 +28,28 @@ github = oauth.remote_app(
 file_details = collections.namedtuple('file_details', 'path, sha')
 
 
+def log_error(message, url, resp, **kwargs):
+    """
+    Log an error from a request and include URL, response status, response data
+    and additional error information
+
+    :params message: Message to log
+    :param url: URL of request that failed
+    :param resp: Response object holding failure information
+    :param kwargs: Additional data to put in error message
+    :returns: None
+    """
+
+    additional_info = []
+    if kwargs:
+        for key, value in kwargs.iteritems():
+            additional_info.append('%s: "%s"' % (key, value))
+
+    app.logger.error('%s at "%s", status: %d, data: %s, %s',
+                     message, url, resp.status, getattr(resp, 'data', None),
+                     ','.join(additional_info))
+
+
 def files_from_github(repo, filename, limit=None):
     """
     Iterate through files with a specific name from github
@@ -43,14 +65,16 @@ def files_from_github(repo, filename, limit=None):
     if sha is None:
         raise StopIteration
 
-    resp = github.get('repos/%s/git/trees/%s?recursive=1' % (repo, sha))
+    url = 'repos/%s/git/trees/%s?recursive=1' % (repo, sha)
+    resp = github.get(url)
 
     if resp.status != 200:
-        # FIXME: Raise exception
+        log_error('Failed reading files', url, resp)
         raise StopIteration
 
     # FIXME: Handle this scenario
-    assert not resp.data['truncated'], 'Too many files for API call'
+    if resp.data['truncated']:
+        log_error('Too many files for API call', url, resp)
 
     count = 0
     for obj in resp.data['tree']:
@@ -72,8 +96,11 @@ def repo_sha_from_github(repo, branch='master'):
     :returns: Sha of branch
     """
 
-    resp = github.get('repos/%s/git/refs/heads/%s' % (repo, branch))
+    url = 'repos/%s/git/refs/heads/%s' % (repo, branch)
+    resp = github.get(url)
+
     if resp.status != 200:
+        log_error('Failed reading sha', url, resp, branch=branch)
         return None
 
     return resp.data['object']['sha']
@@ -133,6 +160,8 @@ def rendered_markdown_from_github(path, branch='master'):
     if resp.status == 200:
         return unicode(resp.data, encoding='utf-8')
 
+    log_error('Failed reading rendered markdown', url, resp, branch=branch)
+
     return None
 
 
@@ -157,6 +186,8 @@ def file_details_from_github(path, branch='master'):
         link = resp.data['_links']['html']
         text = unicode(base64.b64decode(resp.data['content'].encode('utf-8')),
                        encoding='utf-8')
+    else:
+        log_error('Failed reading file details', url, resp, branch=branch)
 
     return (text, sha, link)
 
@@ -207,12 +238,14 @@ def read_user_from_github(username=None):
     """
 
     if username is not None:
-        resp = github.get('users/%s' % (username))
+        url = 'users/%s' % (username)
     else:
-        resp = github.get('user')
+        url = 'user'
+
+    resp = github.get(url)
 
     if resp.status != 200:
-        # FIXME: Handle error
+        log_error('Failed reading user', url, resp)
         return {}
 
     return resp.data
@@ -220,6 +253,8 @@ def read_user_from_github(username=None):
 
 @github.tokengetter
 def get_github_oauth_token():
+    """Read github token from session"""
+
     token = session.get('github_token')
     if token is None:
         # The flask-oauthlib API expects the access token to be in a tuple or a
@@ -269,8 +304,12 @@ def read_branch(repo_path, name):
     :returns: SHA of HEAD and HTTP failure status code
     """
 
-    resp = github.get('repos/%s/git/refs/heads/%s' % (repo_path, name))
+    url = 'repos/%s/git/refs/heads/%s' % (repo_path, name)
+    resp = github.get(url)
+
     if resp.status != 200:
+        log_error('Failed reading branch', url, resp)
+
         return (None, resp.status)
 
     return (resp.data['object']['sha'], resp.status)
@@ -297,13 +336,16 @@ def create_branch(repo_path, name, sha):
 
     if resp.status == 422:
         # Maybe it already exists
-        sha, status = read_branch(repo_path, name)
-        if sha is not None:
+        curr_sha, status = read_branch(repo_path, name)
+        if curr_sha is not None:
             return True
+
+        log_error('Failed reading existing branch', url, resp, sha=sha)
 
         return False
 
     elif resp.status != 201:
+        log_error('Failed creating branch', url, resp, sha=sha)
         return False
 
     return True
@@ -327,6 +369,7 @@ def update_branch(repo_path, name, sha):
     token = (app.config['REPO_OWNER_ACCESS_TOKEN'], )
     resp = github.patch(url, data=data, format='json', token=token)
     if resp.status != 200:
+        log_error('Failed updating branch', url, resp, sha=sha)
         return False
 
     return True
