e64feb6163f6b13405cc7a17620d69859c32e033
Luke Lee <durdenmisc@gmail.com>
2016-01-14 11:02:49 +0100

    Add stacks to article API and model
    - Removed the unused 'language' place holder since we're using the term
      'stacks' everywhere.

--- pskb_website/models/article.py
+++ pskb_website/models/article.py
@@ -159,7 +159,7 @@ def read_article(path, rendered_text=True, branch='master', repo_path=None):
 
 def save_article(title, path, message, new_content, author_name, email, sha,
                  branch='master', image_url=None, repo_path=None,
-                 author_real_name=None):
+                 author_real_name=None, stacks=None):
     """
     Create or save new (original) article, not branched article
 
@@ -176,6 +176,7 @@ def save_article(title, path, message, new_content, author_name, email, sha,
     :param image_url: Image to use for article
     :param repo_path: Optional repo path to save into (<owner>/<name>)
     :param author_real_name: Optional real name of author, not username
+    :param stacks: Optional list of stacks to associate with article
 
     :returns: Article object updated or saved
 
@@ -186,7 +187,8 @@ def save_article(title, path, message, new_content, author_name, email, sha,
     """
 
     article = Article(title, author_name, branch=branch, image_url=image_url,
-                      repo_path=repo_path, author_real_name=author_real_name)
+                      repo_path=repo_path, author_real_name=author_real_name,
+                      stacks=stacks)
     if path:
         article.path = path
 
@@ -244,12 +246,13 @@ def branch_article(article, message, new_content, author_name, email,
 
     return save_article(article.title, article.path, message, new_content,
                         author_name, email, article.sha, branch=branch,
-                        image_url=image_url, author_real_name=author_real_name)
+                        image_url=image_url, author_real_name=author_real_name,
+                        stacks=article.stacks)
 
 
 def branch_or_save_article(title, path, message, content, author_name, email,
                            sha, image_url, repo_path=None,
-                           author_real_name=None):
+                           author_real_name=None, stacks=None):
     """
     Save article as original or as a branch depending on if given author is
     the same as original article (if it already exists)
@@ -267,6 +270,8 @@ def branch_or_save_article(title, path, message, content, author_name, email,
     :param image_url: Image to use for article
     :param repo_path: Optional repo path to save into (<owner>/<name>)
     :param author_real_name: Optional real name of author, not username
+    :param stacks: Optional list of stacks to associate with article (this
+                   argument is ignored if article is branched)
 
     :returns: Article object updated, saved, or branched
     """
@@ -278,12 +283,14 @@ def branch_or_save_article(title, path, message, content, author_name, email,
         article = None
 
     if article and article.author_name != author_name and sha:
+        # Note branching an article cannot change the stacks!
         new = branch_article(article, message, content, author_name, email,
                              image_url, author_real_name=author_real_name)
     else:
         new = save_article(title, path, message, content, author_name, email,
                            sha, image_url=image_url, repo_path=repo_path,
-                           author_real_name=author_real_name)
+                           author_real_name=author_real_name,
+                           stacks=stacks)
 
     return new
 
@@ -415,7 +422,7 @@ class Article(object):
     """
 
     def __init__(self, title, author_name, filename=ARTICLE_FILENAME,
-                 repo_path=None, branch='master', language=None, sha=None,
+                 repo_path=None, branch='master', stacks=None, sha=None,
                  content=None, external_url=None, image_url=None,
                  author_real_name=None):
         """
@@ -426,7 +433,7 @@ class Article(object):
         :param filename: Name of file to save article in
         :param repo_path: Path to repository to save article in
         :param branch: Branch to save article to
-        :param language: Language/stack article primarily covers
+        :param stacks: Language/stack article primarily covers
         :param sha: Git SHA of article (if article already exists in repo)
         :param content: Contents of article
         :param external_url: External URL to view article at
@@ -436,7 +443,7 @@ class Article(object):
 
         self.title = title
         self.author_name = author_name
-        self.language = language
+        self.stacks = stacks or []
         self.content = content
         self.external_url = external_url
         self.filename = filename
@@ -458,10 +465,6 @@ class Article(object):
         self.branches = []
 
         self.path = '%s/%s' % (utils.slugify(self.title), self.filename)
-
-        if self.language is not None:
-            self.path = '%s/%s' % (self.language, self.path)
-
         self.published = False
 
     def __repr__(self):
