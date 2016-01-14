5568220dbd11a03fda6ca563c5e3eada03662418
Luke Lee <durdenmisc@gmail.com>
2016-01-14 11:04:06 +0100

    Add optional stacks choice on article creation
    - Currently this is only shown on the article page in a small, ugly way.  Soon
      it will be used for filtering, etc.

--- pskb_website/forms.py
+++ pskb_website/forms.py
@@ -1,35 +1,35 @@
 from flask_wtf import Form
 from wtforms import StringField, SelectMultipleField, validators
 
-class SignupForm(Form):
-    stack_options = ('Java and J2EE',
-                     'C/C++',
-                     'Python',
-                     'PHP',
-                     'Ruby',
-                     'Ruby on Rails',
-                     'Node.js',
-                     'SQL',
-                     'HTML/CSS',
-                     'Lua',
-                     'Go',
-                     'Scala',
-                     'Haskell',
-                     'Clojure',
-                     'Erlang',
-                     'Objective C',
-                     'Swift',
-                     'Software Engineering Best Practices',
-                     'Microsoft.NET (C#, ASP.NET, VB.NET, etc)',
-                     'Big Data (Hadoop, Spark, etc)',
-                     'NoSQL Databases (MongoDB, Cassandra, etc)',
-                     'Interesting APIs (SendGrid, Twilio, etc)',
-                     'Front-End JavaScript (Angular, React, Meteor, etc)',
-                     'DevOps (Docker, Nagios, Jenkins, Chef, Puppet, etc)',
-                     'Android')
+STACK_OPTIONS = ('Java and J2EE',
+                 'C/C++',
+                 'Python',
+                 'PHP',
+                 'Ruby, Ruby on Rails',
+                 'Node.js',
+                 'SQL',
+                 'HTML/CSS',
+                 'Lua',
+                 'Go',
+                 'Scala',
+                 'Haskell',
+                 'Clojure',
+                 'Erlang',
+                 'Objective C',
+                 'Swift',
+                 'Software Engineering Best Practices',
+                 'Microsoft.NET (C#, ASP.NET, VB.NET, etc)',
+                 'Big Data (Hadoop, Spark, etc)',
+                 'NoSQL Databases (MongoDB, Cassandra, etc)',
+                 'Interesting APIs (SendGrid, Twilio, etc)',
+                 'Front-End JavaScript (Angular, React, Meteor, etc)',
+                 'DevOps (Docker, Nagios, Jenkins, Chef, Puppet, etc)',
+                 'Android')
 
+
+class SignupForm(Form):
     email = StringField('Email', [
         validators.required(),
         validators.Email(message=(u'Please enter a valid email address.')),
     ])
-    stacks = SelectMultipleField('Stacks', choices=[(s, s) for s in stack_options])
+    stacks = SelectMultipleField('Stacks', choices=[(s, s) for s in STACK_OPTIONS])
