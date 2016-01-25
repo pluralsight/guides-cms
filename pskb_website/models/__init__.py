69dd358f109b397d81a41467dbc483d622873d81
Luke Lee <durden2.0@gmail.com>
2016-01-25 10:54:51 +0100

    Add remote and model functions to remove article from repo

--- pskb_website/models/__init__.py
+++ pskb_website/models/__init__.py
@@ -5,6 +5,7 @@ Public model API
 from .article import get_available_articles
 from .article import read_article
 from .article import save_article
+from .article import delete_article
 from .article import branch_article
 from .article import branch_or_save_article
 from .article import get_articles_for_author
