ecf64636a168ab0447debabec433fc1fa6703424
Luke Lee <durden2.0@gmail.com>
2016-02-16 16:54:06 +0100

    Revert "Workaround for error with dropzone and bootstrap-editor"
    This reverts commit 238edef0126b18ee033f0359259d0182d9b1dbab.

--- pskb_website/static/js/vendor/editor/dropzone.js
+++ pskb_website/static/js/vendor/editor/dropzone.js
@@ -437,7 +437,6 @@
         this.options.url = this.element.getAttribute("action");
       }
       if (!this.options.url) {
-          return null;
         throw new Error("No URL provided.");
       }
       if (this.options.acceptedFiles && this.options.acceptedMimeTypes) {
