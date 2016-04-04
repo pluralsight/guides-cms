d0461fa033bdca4fffeff718219f8b71123449d7
Luke Lee <durden2.0@gmail.com>
2016-04-04 11:18:52 +0200

    Remove some functions from exported model API that are not used outside model layer
    - Just some refactoring to trim down the number of things exported that aren't
      necessary at this time.

--- pskb_website/models/__init__.py
+++ pskb_website/models/__init__.py
@@ -11,16 +11,12 @@ from .article import branch_article
 from .article import branch_or_save_article
 from .article import get_articles_for_author
 from .article import get_public_articles_for_author
-from .article import save_article_meta_data
 from .article import find_article_by_title
 from .article import change_article_stack
 
 from .file import read_file
 from .file import read_redirects
 from .file import update_article_listing
-from .file import published_articles
-from .file import in_review_articles
-from .file import draft_articles
 
 from .user import find_user
 
