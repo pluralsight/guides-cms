eeef5e227b400810bc9b821ab8e72a3f3e4f55b1
paulocheque <paulocheque@gmail.com>
2016-04-13 00:46:27 -0300

    Added an anchor link for every header like GitHub

--- pskb_website/static/js/editor_utils.js
+++ pskb_website/static/js/editor_utils.js
@@ -184,7 +184,15 @@ function initialize_editor(local_filename, content, name, real_name, img_upload_
     // editor.renderer.setShowGutter(false);
     // editor.renderer.setOption('showLineNumbers', false);
 
+    var renderer = new marked.Renderer();
+    renderer.heading = function (text, level) {
+        var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+        return '<h' + level + '><a name="' + escapedText + '" class="anchor" href="#' + escapedText + '">' +
+                '<span class="header-link"></span></a>' + text + '</h' + level + '>';
+    };
+
     marked.setOptions({
+      renderer: renderer,
       gfm: true,
       tables: true,
       breaks: true,
