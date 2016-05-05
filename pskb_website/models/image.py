e5fa655babab1c2077b70d2723b6b8a0e525e6fb
Luke Lee <durden2.0@gmail.com>
2016-05-05 14:31:02 +0200

    Set author name correctly when uploading images via API
    - Fixes #66.

--- pskb_website/models/image.py
+++ pskb_website/models/image.py
@@ -64,8 +64,8 @@ def save_image(file_, extension, message, name, email, branch='master'):
     :returns: Public URL to image or None if not successfully saved
     """
 
-    name = secure_filename('%s%s%s' % (str(uuid.uuid4()), os.extsep, extension))
-    path = os.path.join(main_image_path(), name)
+    file_name = secure_filename('%s%s%s' % (str(uuid.uuid4()), os.extsep, extension))
+    path = os.path.join(main_image_path(), file_name)
     url = None
 
     if commit_image_to_github(path, message, file_, name, email,
