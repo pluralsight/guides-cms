pluralsight/guides-cms

commit e5fa655babab1c2077b70d2723b6b8a0e525e6fb

Set author name correctly when uploading images via API
- Fixes #66.

pskb_website/models/image.py
@@ -64,8 +64,8 @@ def save_image(file_, extension, message, name, email, branch='master'):
     :returns: Public URL to image or None if not successfully saved
     """
 
-    name = secure_filename('%s%s%s' % (str(uuid.uuid4()), os.extsep, extension))
-    path = os.path.join(main_image_path(), name)
+    file_name = secure_filename('%s%s%s' % (str(uuid.uuid4()), os.extsep, extension))
+    path = os.path.join(main_image_path(), file_name)
     url = None
 
     if commit_image_to_github(path, message, file_, name, email,

commit d3c76965664de6be18a3e1add15eaa461e060819

Fix broken image url upload link, broken in e5fa655b

pskb_website/models/image.py
@@ -71,6 +71,6 @@ def save_image(file_, extension, message, name, email, branch='master'):
     if commit_image_to_github(path, message, file_, name, email,
                               branch=branch) is not None:
 
-        url = github_url_from_upload_path(path, name, branch=branch)
+        url = github_url_from_upload_path(path, file_name, branch=branch)
 
     return url