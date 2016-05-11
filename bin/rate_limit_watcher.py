09dd03348a6aa22b38beefac25ab412c4d396ccb
Luke Lee <durden2.0@gmail.com>
2016-05-11 09:33:12 +0200

    Add optional ability to log rate limit information to New Relic Insights

--- bin/rate_limit_watcher.py
+++ bin/rate_limit_watcher.py
@@ -3,20 +3,32 @@
 """
 Script to print out Github API rate limit for REPO_OWNER user i.e. the main
 github user account used for the guides-cms application.
+
+This script will automatically report the rate limit data to the New Relic APM
+service IF you have New Relic properly configured in your environment
+variables.  See the New Relic help for how to properly set this up.
 """
 
 import argparse
 from datetime import datetime
+import os
+
 import requests
 
 DOMAIN = 'http://tutorials.pluralsight.com/'
 URL = '/gh_rate_limit'
+BASE_NEW_RELIC_EVENTS_URL = 'https://insights-collector.newrelic.com/v1/accounts'
 
 
-def main(domain):
+def main(domain, report_to_new_relic=False):
     response = get_rate_limit(domain)
-    if response:
-        pprint(response)
+    if not response:
+        return
+
+    pprint(response)
+
+    if report_to_new_relic:
+        report_new_relic_custom_events(response)
 
 
 def get_rate_limit(domain=DOMAIN):
@@ -53,7 +65,44 @@ def pprint(rate_limit):
     print_('core', rate_limit['resources'])
     print_('search', rate_limit['resources'])
 
-    #u'resources': {u'core': {u'reset': 1462781427, u'limit': 5000, u'remaining': 4923}, u'search': {u'reset': 1462780271, u'limit': 30, u'remaining': 30}}}
+
+def report_new_relic_custom_events(rate_limit):
+    """
+    Report custom metrics for API requests used and limit with Github API to
+    New Relic
+    """
+
+    acct_id = os.environ.get('NEW_RELIC_ACCT_ID')
+    key = os.environ.get('NEW_RELIC_INSIGHTS_API_KEY')
+    if acct_id is None or key is None:
+        print 'Unable to sent events to New Relic without NEW_RELIC_ACCT_ID and NEW_RELIC_INSIGHT_API_KEY environment variables'
+        return
+
+    # Report used instead of remaining just to make bar graphs more
+    # readable by seeing if there's a big gap between what's remaining.
+    limit = rate_limit['resources']['core']['limit']
+    used = limit - rate_limit['resources']['core']['remaining']
+    percent_used = used / float(limit)
+
+    event = {'eventType': 'GithubAPI', 'limit': limit, 'used': used,
+             'percent_used': percent_used}
+
+    # https://docs.newrelic.com/docs/insights/new-relic-insights/adding-querying-data/inserting-custom-events-insights-api
+
+    url = '%s/%s/events' % (BASE_NEW_RELIC_EVENTS_URL, acct_id)
+    headers = {'X-Insert-Key': key}
+
+    resp = requests.post(url, json=event, headers=headers)
+    if resp.status_code != 200:
+        print 'Failed reporting to New Relic, status_code: %d' % (resp.status_code)
+
+    # Tried using the newrelic custom events but always got errors about agent
+    # not running, probably because we're running from a script and not the
+    # actual wsgi app.
+    #agent.record_custom_event('GithubAPI',
+                              #{'Calls Limit': limit, 'Calls Used': used},
+                              #agent.application())
+
 
 def _parse_args():
     """Parse args and get dictionary back"""
@@ -62,10 +111,14 @@ def _parse_args():
     parser.add_argument('-d', '--domain', action='store', required=False,
                         default=DOMAIN,
                         help='Domain to ping for rate limit JSON response (default: %s)' % (DOMAIN))
+    parser.add_argument('--report-to-new-relic', action='store_true',
+                        default=False, dest='report_to_new_relic',
+                        help='Enable automatic reporting to New Relic APM when environment variables are configured')
 
     # Turn odd argparse namespace object into a plain dict
     return vars(parser.parse_args())
 
 
 if __name__ == '__main__':
-    main(_parse_args()['domain'])
+    args = _parse_args()
+    main(args['domain'], args['report_to_new_relic'])
--- docs/github_api_usage.rst
+++ docs/github_api_usage.rst
@@ -1,3 +1,62 @@
 ================
 Github API usage
 ================
+
+The CMS heavily uses the Github API.  All of the raw API interaction takes
+places in :file:`pskb_website/remote.py`.
+
+-----------------------
+Logging API Rate Limits
+-----------------------
+
+The CMS uses authenticated API requests to ensure a higher rate limit, which
+at the time of this writing is `5000 requests/hour <https://developer.github.com/v3/#rate-limiting>`_.  This is sufficient if caching and
+`conditional requests <https://developer.github.com/v3/#conditional-requests>`_
+are used.
+
+It's worth noting that the limit is per user and per application.  Therefore,
+the CMS can make 5000 API requests/hour on behalf of a user.  However, not all
+requests can be made with a specific user.  For example, all requests to commit
+data to the content repository use the `REPO_OWNER` Github API account.  This
+is necessary because regular users do not have commit rights to the content
+repository.  The requests using the `REPO_OWNER` Github API account are:
+
+- Reading guides for a non-logged in user
+- Committing guides to any branch
+- Uploaded images for a guide
+
+The `REPO_OWNER` account is the only account reasonably affected by the rate
+limiting because typical usage will not lead to a logged-in user reading
+thousands of guides in an hour.
+
+It can be useful to monitor your usage since there's an upper limit.  You can
+log the CMS' Github API usage with the `bin/rate_limit_watcher.py` script.
+There are a few ways to automate this data collection.
+
+Heroku Scheduler
+----------------
+
+You can use the `Heroku Scheduler <https://devcenter.heroku.com/articles/scheduler>`_
+add on to run `bin/rate_limit_watcher.py`.  Just add this add-on to your
+account and set the add-on to run `bin/rate_limit_watcher.py` with your own
+arguments.
+
+New Relic Insights
+------------------
+
+You can also graph your API usage overtime by using `Custom Events from New Relic <https://docs.newrelic.com/docs/insights/new-relic-insights/adding-querying-data/inserting-custom-events-insights-api>`_.
+To do this you'll need to configure a few environment variables for your setup:
+
+- `NEW_RELIC_ACCT_ID` - Your New Relic account ID
+- `NEW_RELIC_INSIGHTS_API_KEY` - Your New Relic Insights API Key
+
+You can get help finding these values by using `the official New Relic docs <https://docs.newrelic.com/docs/insights/new-relic-insights/adding-querying-data/inserting-custom-events-insights-api>`_.
+
+Finally, run `bin/rate_limit_watcher.py --report-to-new-relic` to log your
+usage to New Relic.
+
+We're using the New Relic Insights API instead of New Relic custom metrics and
+custom events because the timestamps of the Github API data is not that
+critical.  The API limits do not need to be synchonized with all the other New
+Relic data.  In addition, the Insights API is easier to use from a script
+that's not embedded in the main WSGI application.
